c52d20c42642e0939cb9aa1bbc87de27c6800192
Fawadul Hasan <rahathasan706@gmail.com>
2019-04-15 17:57:17 +0600

    Adds change in rotateArray solution

--- LearnProgramming/RotateArray.cpp
+++ LearnProgramming/RotateArray.cpp
@@ -3,46 +3,33 @@
 
 using namespace std;
 
-//void rotate(vector<int>& nums, int k) {
-//	vector<int> numsNew;
-//	for (auto i = 0; i < k; i++) {
-//		int temp = *nums.end();
-//		numsNew.push_back(temp);
-//		nums.pop_back();
-//	}
-//
-//	for (auto i = nums.begin(); i < nums.end(); i++) {
-//		numsNew.push_back(*i);
-//	}
-//	
-//	for (auto i = numsNew.begin(); i < numsNew.end(); i++) {
-//		cout << *i;
-//	}
-//}
+void rotate(int nums[], int k) {
+	int numsNew[5];
+	int size = sizeof(nums);
 
-void rotate(vector<int>& nums, int k) {
-	int size = nums.size();
-
-	for (int i = 0; i < k; i++) {
-		int temp = nums[size - i];
-		int temp2 = nums[1 + i];
-		nums.insert(size - i,temp2);
-		nums[1 + i] = temp;
+	for (int i = size - k; i <= size; i++) {
+		int j = 0;
+		numsNew[j] = nums[i];
+		j++;
 	}
 
-	for (int j = 0; j < size; j++) {
-		cout << nums[j] << " ";
+	//int newSize = sizeof(numsNew);
+
+	for (int i = 0; i < size - k; i++) {
+		numsNew[k] = nums[i];
+		k++;
 	}
+
+	for (int i = 0; i < 5; i++) {
+		cout << numsNew[i] << " ";
+	}
+
 }
 
 int main() {
 	int number;
-	vector<int> nums;
+	int nums[5] = { 0, 1, 2, 3, 4 };
 	cout << "Enter the number of k: ";
-	cin >> number;
-	for (int i = 0; i < 5; i++) {
-		nums.push_back(i);
-	}
-	
+	cin >> number;	
 	rotate(nums, number);
 }
