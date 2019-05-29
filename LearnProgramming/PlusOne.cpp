6077ad1c48afc21cfe55cf6bbca1c8d3c5676ca4
Fawadul Hasan <rahathasan706@gmail.com>
2019-05-30 02:23:36 +0600

    - Adds solution to plus one problem

--- LearnProgramming/PlusOne.cpp
+++ LearnProgramming/PlusOne.cpp
@@ -4,28 +4,34 @@
 using namespace std;
 
 vector<int> plusOne(vector<int>& digits) {
-	(*(digits.end() - 1))++;
-	int last = (*(digits.end() - 1))++;
-
-	if (last <= 10) {
-		vector<int> temp;
-		while (last > 0) {
-			int remainder = last % 10;
-			temp.push_back(remainder);
-			last = last / 10;
-		}
-		reverse(temp.begin(),temp.end());
-		digits.pop_back();
-		for (auto i = temp.begin(); i != temp.end(); i++)
-			digits.push_back(*i);
+	long number = 0;
+	for (auto i = digits.begin(); i != digits.end(); i++) {
+		number = (*i) + number * 10;
 	}
-
-	for (auto i = digits.begin(); i != digits.end(); i++)
+	if ((*(digits.end() - 1)) == 0) {
+		number = number * 10;
+	}
+	number++;
+	vector<int> temp;
+	if (number > 10) {		
+		while (number > 0) {
+			long remainder = number % 10;
+			temp.push_back(remainder);
+			number = number / 10;
+		}
+		reverse(temp.begin(), temp.end());
+	}
+	digits.clear();
+	for (auto i = temp.begin(); i != temp.end(); i++) {
+		digits.push_back(*i);
+	}
+	for (auto i = digits.begin(); i != digits.end(); i++) {
 		cout << *i << " ";
+	}
 	return digits;
 }
 
 int main() {
-	vector<int> inputArray{ 9,9};
+	vector<int> inputArray{9,8,7,6,5,4,3,2,1,0};
 	plusOne(inputArray);
 }
