53e0b448f4d196f3dc74ec1b2beefc909236ab5a
Fawadul Hasan <rahathasan706@gmail.com>
2019-06-03 02:16:31 +0600

    - Adds solution to plus pne

--- LearnProgramming/PlusOne.cpp
+++ LearnProgramming/PlusOne.cpp
@@ -4,34 +4,41 @@
 using namespace std;
 
 vector<int> plusOne(vector<int>& digits) {
-	long number = 0;
-	for (auto i = digits.begin(); i != digits.end(); i++) {
-		number = (*i) + number * 10;
-	}
-	if ((*(digits.end() - 1)) == 0) {
-		number = number * 10;
-	}
-	number++;
+	(*(digits.end() - 1))++;
+	int carry = 0;
+	
+	reverse(digits.begin(), digits.end());
+	int last = (*digits.begin());
 	vector<int> temp;
-	if (number > 10) {		
-		while (number > 0) {
-			long remainder = number % 10;
-			temp.push_back(remainder);
-			number = number / 10;
+
+	if (last == 10) {		
+		for (auto i = digits.begin(); i != digits.end(); i++) {
+			if (*i == 10) {
+				temp.push_back(0);
+				carry++;
+			}
+			else {
+				temp.push_back(*i);
+			}
+			if (carry > 0) {
+				(*(i + 1))++;
+				carry--;
+			}
 		}
-		reverse(temp.begin(), temp.end());
 	}
-	digits.clear();
+	else {
+		for (auto i = digits.begin(); i != digits.end(); i++) {
+			temp.push_back(*i);
+		}
+	}
+	reverse(temp.begin(), temp.end());
 	for (auto i = temp.begin(); i != temp.end(); i++) {
-		digits.push_back(*i);
-	}
-	for (auto i = digits.begin(); i != digits.end(); i++) {
 		cout << *i << " ";
 	}
-	return digits;
+	return temp;
 }
 
 int main() {
-	vector<int> inputArray{9,8,7,6,5,4,3,2,1,0};
+	vector<int> inputArray{8,9};
 	plusOne(inputArray);
 }
