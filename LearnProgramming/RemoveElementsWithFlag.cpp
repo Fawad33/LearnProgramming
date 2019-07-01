bc151dab071a36b92d43786dd607991d8cd4ebd9
Fawadul Hasan <rahathasan706@gmail.com>
2019-07-02 01:21:45 +0600

    - Adds Remove Element With Count solution

--- LearnProgramming/RemoveElementsWithFlag.cpp
+++ LearnProgramming/RemoveElementsWithFlag.cpp
@@ -1,32 +1,32 @@
-#include<iostream>
-#include<vector>
-
-using namespace std;
-
-int removeElement(vector<int>& nums, int val) {
-	int i = 0;
-	if (nums.size() == 0)
-		return 0;
-	bool flag = true;
-	while (flag == true) {
-		flag = false;
-		for (auto i = nums.begin(); i < nums.end();) {
-			if (*i == val) {
-			nums.erase(i);
-			flag = true;
-			break;
-			}
-			else
-				i++;
-		}
-	}
-	cout << nums.size();
-	return nums.size();
-}
-
-int main() {
-	vector<int> nums{ 3,2,2,3 };
-	int value = 3;
-	removeElement(nums, value);
-	return 0;
-}
+//#include<iostream>
+//#include<vector>
+//
+//using namespace std;
+//
+//int removeElement(vector<int>& nums, int val) {
+//	int i = 0;
+//	if (nums.size() == 0)
+//		return 0;
+//	bool flag = true;
+//	while (flag == true) {
+//		flag = false;
+//		for (auto i = nums.begin(); i < nums.end();) {
+//			if (*i == val) {
+//			nums.erase(i);
+//			flag = true;
+//			break;
+//			}
+//			else
+//				i++;
+//		}
+//	}
+//	cout << nums.size();
+//	return nums.size();
+//}
+//
+//int main() {
+//	vector<int> nums{ 3,2,2,3 };
+//	int value = 3;
+//	removeElement(nums, value);
+//	return 0;
+//}
