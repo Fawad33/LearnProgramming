03fffdde67b997d186e2098e208cdfacedffb647
Fawadul Hasan <rahathasan706@gmail.com>
2019-07-05 01:45:27 +0600

    - Adds solution to remove elements

--- LearnProgramming/RemoveElementWithCount.cpp
+++ LearnProgramming/RemoveElementWithCount.cpp
@@ -1,34 +1,34 @@
-#include<iostream>
-#include<vector>
-
-using namespace std;
-
-int removeElement(vector<int>& nums, int val) {
-	int count = 0;
-	if (nums.size() == 0)
-		return 0;
-
-	for (auto i = nums.begin(); i != nums.end(); i++) {
-		if (*i == val)
-			count++;
-	}
-	int x = 0;
-	while(x < count) {
-		for(auto i = nums.begin(); i != nums.end(); i++) {
-			if (*i == val) {
-				nums.erase(i);
-				x++;
-				break;
-			}
-		}
-	}
-	cout << nums.size();
-	return nums.size();
-}
-
-int main() {
-	vector<int> nums{ 0,1,2,2,3,0,4,2 };
-	int value = 2;
-	removeElement(nums, value);
-	return 0;
-}
+//#include<iostream>
+//#include<vector>
+//
+//using namespace std;
+//
+//int removeElement(vector<int>& nums, int val) {
+//	int count = 0;
+//	if (nums.size() == 0)
+//		return 0;
+//
+//	for (auto i = nums.begin(); i != nums.end(); i++) {
+//		if (*i == val)
+//			count++;
+//	}
+//	int x = 0;
+//	while(x < count) {
+//		for(auto i = nums.begin(); i != nums.end(); i++) {
+//			if (*i == val) {
+//				nums.erase(i);
+//				x++;
+//				break;
+//			}
+//		}
+//	}
+//	cout << nums.size();
+//	return nums.size();
+//}
+//
+//int main() {
+//	vector<int> nums{ 0,1,2,2,3,0,4,2 };
+//	int value = 2;
+//	removeElement(nums, value);
+//	return 0;
+//}
---
+++ LearnProgramming/RemoveElements.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int removeElement(vector<int>& nums, int val) {
+	if (nums.size() == 0)
+		return 0;
+
+	auto it = nums.begin();
+	while (it != nums.end()) {
+		if (*it == val) {
+			it = nums.erase(it);
+		}
+		else {
+			it++;
+		}
+	}
+
+	cout << nums.size();
+	return nums.size();
+}
+
+int main() {
+	vector<int> nums{ 0,1,2,2,3,0,4,2 };
+	int value = 2;
+	removeElement(nums, value);
+	return 0;
+}
