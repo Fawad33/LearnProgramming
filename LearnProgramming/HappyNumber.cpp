b6dab0e16b4d2cd740bc19a1f6fa13c4916e608f
Fawadul Hasan <rahathasan706@gmail.com>
2019-08-19 01:48:59 +0600

    - Adds solution to HappyNumberWithHashing

--- LearnProgramming/HappyNumber.cpp
+++ LearnProgramming/HappyNumber.cpp
@@ -1,41 +1,41 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-
-using namespace std;
-vector<int> v;
-
-bool ishappy(int n){
-	int sum = 0, ntemp = n;	
-	if (n == 1){
-		return true;
-	}
-	while (n > 0){
-		sum += pow(n % 10,2);
-		n = n / 10;
-	}
-
-	v.push_back(ntemp);
-
-	for (auto i = v.begin(); i != v.end(); i++){
-		if (*i == sum){
-			return false;
-		}
-		
-	}
-	return ishappy(n);
-	
-}
+//#include<iostream>
+//#include<vector>
+//#include<algorithm>
+//
+//using namespace std;
+//vector<int> v;
+//
+//bool ishappy(int n){
+//	int sum = 0, ntemp = n;	
+//	if (n == 1){
+//		return true;
+//	}
+//	while (n > 0){
+//		sum += pow(n % 10,2);
+//		n = n / 10;
+//	}
+//
+//	v.push_back(ntemp);
+//
+//	for (auto i = v.begin(); i != v.end(); i++){
+//		if (*i == sum){
+//			return false;
+//		}
+//		
+//	}
+//	return ishappy(n);
+//	
+//}
 
 
 //void main(){
 //	int num;
-//	cout << "Enter a number: ";
+//	cout << "enter a number: ";
 //	cin >> num;
-//	if (isHappy(num) == true){
-//		cout << "True";
+//	if (ishappy(num) == true){
+//		cout << "true";
 //	}
 //	else{
-//		cout << "False";
+//		cout << "false";
 //	}
 //}
--- LearnProgramming/arrayOfPointers.cpp
+++ LearnProgramming/arrayOfPointers.cpp
@@ -1,10 +1,10 @@
-#include<iostream>
-
-using namespace std;
-
-int main() {
-	const char* arr[2] = {"Hello", "World!"};
-	for (int i = 0; i < 2; i++) {
-		cout << (arr + i) << " ";
-	}
-}
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	const char* arr[2] = {"Hello", "World!"};
+//	for (int i = 0; i < 2; i++) {
+//		cout << (arr + i) << " ";
+//	}
+//}
