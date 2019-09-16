e7f32d0850327277e007d5a93bd5faf47572518f
Fawadul Hasan <rahathasan706@gmail.com>
2019-09-16 21:57:23 +0600

    - Adds solution to FrequencyCountOfChar solution

---
+++ LearnProgramming/FrequencyCountOfChar.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<map>
+#include<string>
+
+using namespace std;
+
+void frequencyCountOfChar(string str) {
+	map<char, int> characterMap;
+	for (auto stringIterator = str.begin(); stringIterator != str.end(); stringIterator++) {
+		map<char, int>::iterator mapIterator;
+		mapIterator = characterMap.find(*stringIterator);
+		if (mapIterator != characterMap.end())
+			mapIterator->second++;
+		else
+			characterMap.insert({*stringIterator, 1});
+	}
+
+	for (auto i = characterMap.begin(); i != characterMap.end(); i++)
+		cout << i->first << " occurred " << i->second << " times\n";
+}
+
+int main() {
+	string str = "aaaaabcddddddddd";
+	frequencyCountOfChar(str);
+	return 0;
+}
--- LearnProgramming/FrequencyCountOfInt.cpp
+++ LearnProgramming/FrequencyCountOfInt.cpp
@@ -9,19 +9,17 @@ void frequencyCount(vector<int> v) {
 	for (auto i = v.begin(); i != v.end(); i++) {
 		std::map<int, int> ::iterator j;
 		j = frequency.find(*i);
-		if (j != frequency.end()) {
-			j->second++;
-		}
-		else {
+		if (j != frequency.end()) 
+			j->second++;		
+		else
 			frequency.insert({ *i, 1 });
-		}
 	}
 
 	for (auto i = frequency.begin(); i != frequency.end(); i++)
 		cout << i->first << " Occurred " << i->second << " times\n";
 }
 
-int main() {
-	vector<int> v = {1, 2, 2, 3, 3, 3, 3, 5};
-	frequencyCount(v);
-}
+//int main() {
+//	vector<int> v = {1, 2, 2, 3, 3, 3, 3, 5};
+//	frequencyCount(v);
+//}
