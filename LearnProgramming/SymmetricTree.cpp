913df89f37d5a32df14260da5859e582f446c5e7
Fawadul Hasan <rahathasan706@gmail.com>
2019-12-01 17:45:52 +0600

    - Adds solution to DFS

--- LearnProgramming/SymmetricTree.cpp
+++ LearnProgramming/SymmetricTree.cpp
@@ -36,15 +36,15 @@ class SymmetricTree {
 	}
 };
 
-void main() {
-		struct TreeNode* root = new TreeNode(1);
-		root->left = new TreeNode(2);
-		root->right = new TreeNode(2);
-		root->left->left = new TreeNode(3);
-		root->left->right = new TreeNode(4);
-		root->right->left = new TreeNode(4);
-		root->right->right = new TreeNode(3);
-
-		SymmetricTree symmetricTree;
-		symmetricTree.isSymmetric(root);
-	}
+//void main() {
+//		struct TreeNode* root = new TreeNode(1);
+//		root->left = new TreeNode(2);
+//		root->right = new TreeNode(2);
+//		root->left->left = new TreeNode(3);
+//		root->left->right = new TreeNode(4);
+//		root->right->left = new TreeNode(4);
+//		root->right->right = new TreeNode(3);
+//
+//		SymmetricTree symmetricTree;
+//		symmetricTree.isSymmetric(root);
+//}
