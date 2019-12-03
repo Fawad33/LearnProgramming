981fa38735a0ae3e31db037e41fc98a9d0e2bb31
Fawadul Hasan <rahathasan706@gmail.com>
2019-12-04 00:50:59 +0600

    - Adds change to dfs new

--- LearnProgramming/DepthFirstSearchNew.cpp
+++ LearnProgramming/DepthFirstSearchNew.cpp
@@ -9,8 +9,8 @@ struct Node {
 	Node(int x) : i(x) { }
 };
 struct Edge {
-	struct Node* x;
-	struct Node* y;
+	Node* x;
+	Node* y;
 	Edge(Node* x, Node* y) : x(x), y(y) {}
 };
 
@@ -27,20 +27,20 @@ public:
 };
 
 void main() {
-	struct Node* one = new Node(1);
-	struct Node* two = new Node(2);
-	struct Node* three = new Node(3);
-	struct Node* four = new Node(4);
-	struct Node* five = new Node(5);
-	struct Node* six = new Node(6);
+	Node* one = new Node(1);
+	Node* two = new Node(2);
+	Node* three = new Node(3);
+	Node* four = new Node(4);
+	Node* five = new Node(5);
+	Node* six = new Node(6);
 
-	struct Edge* oneToTwo = new Edge(one, two);
-	struct Edge* oneToFour = new Edge(one, four);
-	struct Edge* twoToFive = new Edge(two, five);
-	struct Edge* threeToFive = new Edge(three, five);
-	struct Edge* threeToSix = new Edge(three, six);
-	struct Edge* fourToTwo = new Edge(four, two);
-	struct Edge* fiveToFour = new Edge(five, four);
-	struct Edge* sixToSix = new Edge(six, six);
+	Edge* oneToTwo = new Edge(one, two);
+	Edge* oneToFour = new Edge(one, four);
+	Edge* twoToFive = new Edge(two, five);
+	Edge* threeToFive = new Edge(three, five);
+	Edge* threeToSix = new Edge(three, six);
+	Edge* fourToTwo = new Edge(four, two);
+	Edge* fiveToFour = new Edge(five, four);
+	Edge* sixToSix = new Edge(six, six);
 	cout << (oneToFour->x)->i;
 }
