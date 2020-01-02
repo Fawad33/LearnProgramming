f2dc18dcbc805bae96550c30c0bba25083429b71
Fawadul Hasan <rahathasan706@gmail.com>
2020-01-02 23:20:44 +0600

    - Adds change to longest common prefix

--- LearnProgramming/LongestCommonPrefix.cpp
+++ LearnProgramming/LongestCommonPrefix.cpp
@@ -7,13 +7,15 @@ using namespace std;
 class LongestCommonPrefix {
 public:
 	string longestCommonPrefix(vector<string>& strs) {
+		if (strs.empty())
+			return "";
 		string maxPrefix = strs[0];
 		string currentPrefix;
 		for (int j = 1; j < strs.size(); j++) {
+			string currentString = strs[j];
 			currentPrefix = "";
-			for (int i = 0; i < maxPrefix.size(); i++) {
-				string tempString = strs[j];
-				if (maxPrefix[i] == tempString[i]) {
+			for (int i = 0; i < maxPrefix.size() && i < currentString.size(); i++) {				
+				if (maxPrefix[i] == currentString[i]) {
 					currentPrefix = currentPrefix + maxPrefix[i];
 				}
 			}
@@ -23,7 +25,7 @@ public:
 };
 
 void main() {
-	vector<string> words = {"cat", "car", "cow"};
+	vector<string> words = { "cat", "cats", "co"};
 	LongestCommonPrefix longestCommonPrefixObject;
 	cout << longestCommonPrefixObject.longestCommonPrefix(words);
 }
