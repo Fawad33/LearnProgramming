a70d1fd08cf76af87a3668ae0552ee41665cf1c7
Fawadul Hasan <rahathasan706@gmail.com>
2020-01-07 23:32:48 +0600

    - adds change to strstr

--- LearnProgramming/ImplementStrStr.cpp
+++ LearnProgramming/ImplementStrStr.cpp
@@ -5,24 +5,23 @@ using namespace std;
 
 class ImplementStrStr {
 public:
-	string strStr(string haystack, string needle) {
+	bool strStr(string haystack, string needle) {
 		if (needle == "" && haystack == "")
-			return 0;
-		string answer;
-		for (int i = 0; i < haystack.size() && i < needle.size(); i++) {
-			if (haystack[i] == needle[i]) {
-				answer.push_back(haystack[i]);
-			}
-			else {
+			return false;
+		int i;
+		for (i = 0; i < haystack.size() && i < needle.size(); i++) {
+			if (haystack[i] != needle[i]) {
 				break;
 			}
 		}
-		return answer;
+		if (i == needle.size())
+			return true;
+		return false;
 	}
 };
 
 int main() {
-	string needle = "apple", haystack = "app";
+	string needle = "app", haystack = "apple";
 	ImplementStrStr implementStrStr;
 	cout << implementStrStr.strStr(haystack, needle);
 }
