d51813fd77d196571a19088b8516f3de5cc3c0a9
Fawadul Hasan <rahathasan706@gmail.com>
2020-01-11 22:50:53 +0600

    - Adds change to merge sorted array solution

--- LearnProgramming/MergeSortedArray.cpp
+++ LearnProgramming/MergeSortedArray.cpp
@@ -6,30 +6,26 @@ using namespace std;
 class MergeSortedArray {
 public:
 	void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-		int i = 0, j = 0;
-		while (j < n) {
-			if (i >= nums1.size() && nums1[i] < nums2[j]) {
-				auto it = nums1.begin();
-				nums1.insert(it + i + 1, nums2[j]);
-				i++, j++;
-			}						
-			else if (nums1[i] < nums2[j] && i + 1 < nums1.size()) {
-				i++;
+		int a = m + n - 1;
+		n--, m--;
+		while (n >= 0) {
+			if (nums1[m] <= nums2[n]) {
+				nums1[a] = nums2[n];
+				n--, a--;
 			}
-			else {
-				auto it = nums1.begin();
-				nums1.insert(it + i +1, nums2[j]);
-				i++, j++;
+			else if (nums1[m] > nums2[n]) {
+				nums1[a] = nums1[m];
+				m--, a--;
 			}
 		}
-		for (auto i = nums1.begin(); i != nums1.end(); i++)
-			cout << *i << " ";
+		for (auto a = nums1.begin(); a != nums1.end(); a++)
+			cout << *a << " ";
 	}
 };
 
 void main() {
 	vector<int> num1 = { 1, 2, 3, 0, 0, 0};
-	vector<int> num2 = { 2, 5, 6 };
+	vector<int> num2 = { 2, 4, 6 };
 	int m = 3, n = 3;
 	MergeSortedArray mergeSortedArrayObject;
 	mergeSortedArrayObject.merge(num1, m, num2, n);
