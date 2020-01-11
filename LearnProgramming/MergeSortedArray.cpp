0f225e1fbeb33b3c4f2c1e438b4f296748faf62c
Fawadul Hasan <rahathasan706@gmail.com>
2020-01-11 23:29:40 +0600

    - adds change to merge sorted array

--- LearnProgramming/MergeSortedArray.cpp
+++ LearnProgramming/MergeSortedArray.cpp
@@ -9,13 +9,19 @@ public:
 		int a = m + n - 1;
 		n--, m--;
 		while (n >= 0) {
-			if (nums1[m] <= nums2[n]) {
+			if (m < 0) {
 				nums1[a] = nums2[n];
 				n--, a--;
 			}
-			else if (nums1[m] > nums2[n]) {
-				nums1[a] = nums1[m];
-				m--, a--;
+			else {
+				if (nums1[m] <= nums2[n]) {
+					nums1[a] = nums2[n];
+					n--, a--;
+				}
+				else {
+					nums1[a] = nums1[m];
+					m--, a--;
+				}
 			}
 		}
 		for (auto a = nums1.begin(); a != nums1.end(); a++)
@@ -24,9 +30,9 @@ public:
 };
 
 void main() {
-	vector<int> num1 = { 1, 2, 3, 0, 0, 0};
-	vector<int> num2 = { 2, 4, 6 };
-	int m = 3, n = 3;
+	vector<int> num1 = { 4, 5, 6, 0, 0, 0, 0, 0 };
+	vector<int> num2 = { 2, 3, 7, 8, 9 };
+	int m = 3, n = 5;
 	MergeSortedArray mergeSortedArrayObject;
 	mergeSortedArrayObject.merge(num1, m, num2, n);
 }
