f865c7ad8d93bef56bf1d604cbf8ed50998c31f1
Fawadul Hasan <rahathasan706@gmail.com>
2020-01-12 22:16:48 +0600

    - Adds change to two sum II

--- LearnProgramming/TwoSumII.cpp
+++ LearnProgramming/TwoSumII.cpp
@@ -8,14 +8,8 @@ public:
 	vector<int> twoSumII(vector<int>& numbers, int target) {
 		vector<int> numsNew;
 		int size = numbers.size();
-		int counter = 0;
 		for (int i = 0; i < size; i++) {
-			counter++;
-			if (numbers[i] > target)
-				break;
-			for (int j = counter; j < size; j++) {
-				if (numbers[j] > target)
-					break;
+			for (int j = i + 1; j < size; j++) {
 				if (numbers[i] + numbers[j] == target) {
 					numsNew.push_back(i + 1);
 					numsNew.push_back(j + 1);
@@ -31,7 +25,7 @@ public:
 
 void main(){
 	vector<int> numbers = { 6, 3, 11, 15 };
-	int target = 11;
+	int target = 9;
 	TwoSumII twoSumII;
 	twoSumII.twoSumII(numbers, target);
 }
