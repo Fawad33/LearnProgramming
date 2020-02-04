02557c7d9df45961e629914188548a4d515a1cbd
Fawadul Hasan <rahathasan706@gmail.com>
2020-02-05 02:06:58 +0600

    - Adds change to valid palindrome solution

--- LearnProgramming/ValidPalindrome.cpp
+++ LearnProgramming/ValidPalindrome.cpp
@@ -9,11 +9,11 @@ public:
 		int length = s.length();
 		int i = 0, j = length - 1;
 		bool palindrome = true;
-		while ((j - i) >= 1) {
-			while ((int(tolower(s[i])) <= 96 || int(tolower(s[i])) > 122) && i < (length - 1)) {
+		while (j > i) {
+			while ((int(s[i]) < '0' || int(s[i]) > '9') && (int(tolower(s[i])) < 'a' || int(tolower(s[i])) > 'z') && i < j ) {
 				i++;
 			}
-			while ((int(tolower(s[j])) <= 96 || int(tolower(s[j])) > 122) && j < (length - 1)) {
+			while ((int(s[j]) < '0' || int(s[j]) > '9') && (int(tolower(s[j])) < 'a' || int(tolower(s[j])) > 'z')  && j > i) {
 				j--;
 			}
 			
@@ -29,7 +29,7 @@ public:
 };
 
 void main() {
-	string input = ".,";
+	string input = "race a car";
 	ValidPalindrome validPalindrome;
 	if (validPalindrome.isPalindrome(input) == true)
 		cout << "true";
