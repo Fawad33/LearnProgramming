db2c31bfe0ef92a9a9c52463e546d29966773546
Fawadul Hasan <rahathasan706@gmail.com>
2020-02-16 18:15:06 +0600

    - Adds solution to Excel Sheet Column Title

--- LearnProgramming/DecimalToBinary.cpp
+++ LearnProgramming/DecimalToBinary.cpp
@@ -10,19 +10,19 @@ public:
 		while ((input / 2) >= 1) {
 			int x = input % 2;
 			input = input / 2;
-			binary = to_string(x) + binary;		
+			binary = to_string(x) + binary;
+		}
 
-			if ((input / 2) == 0 && (input % 2) == 1) {
-				binary = "1" + binary;
-			}
+		if (input <= 1 && (input % 2) == 1) {
+			binary = "1" + binary;
 		}
 		
 		return binary;
 	}
 };
 
-void main() {
-	int input = 71;
-	DeciamlToBinary deciamlToBinaryObject;
-	cout << deciamlToBinaryObject.decimalToBinary(input);
-}
+//void main() {
+//	int input = 71;
+//	DeciamlToBinary deciamlToBinaryObject;
+//	cout << deciamlToBinaryObject.decimalToBinary(input);
+//}
---
+++ LearnProgramming/ExcelSheetColumnTitle.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+using namespace std;
+
+class ExcelSheetColumnTitle {
+public:
+	string convertToTitle(int n) {
+		string result;
+		if (n <= 26) {
+			result += (char)(n + 64);
+		}
+		else if ((n / 26) > 1 && n % 26 == 0) {
+			while ((n / 26) > 1) {
+				int x = n % 26;
+				n = (n / 26) - 1;
+				if (n <= 26) {
+					result += (char)(n + 64);
+				}
+				result += (char)(x + 64 + 26);
+			}
+		}
+		else {
+			while ((n / 26) >= 1) {				
+				int x = n % 26;
+				n = n / 26;
+				if (n <= 26) {
+					result += (char)(n + 64);
+				}
+				result += (char)(x + 64);
+			}
+		}
+		return result;
+	}
+};
+
+void main() {
+	int n = 703;
+	ExcelSheetColumnTitle excelSheetColumnTitleObject;
+	cout << excelSheetColumnTitleObject.convertToTitle(n);
+}
