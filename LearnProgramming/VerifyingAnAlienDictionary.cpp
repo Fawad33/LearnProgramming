badc9d64c0af91f11d5563b68e7d3efe522db233
Fawadul Hasan <rahathasan706@gmail.com>
2020-02-25 23:28:26 +0600

    - Adds change to Verifying an ailen dictionary problem

--- LearnProgramming/VerifyingAnAlienDictionary.cpp
+++ LearnProgramming/VerifyingAnAlienDictionary.cpp
@@ -20,11 +20,21 @@ public:
 		}
 		else {
 			for (int i = 0; i < words.size() - 1; i++) {
+				bool breakFlag = true;
 				for (int j = 0; j < words[i].size() && j < words[i + 1].size(); j++) {
-					if (valueTable[words[i][j]] > valueTable[words[i + 1][j]]) {
+					if (valueTable[words[i][j]] < valueTable[words[i + 1][j]]) {
+						breakFlag = false;
 						break;
 					}
-					
+					else if (valueTable[words[i][j]] > valueTable[words[i + 1][j]]) {
+						result = false;
+						breakFlag = false;
+						break;
+					}
+
+					if ((words[i].size() > words[i + 1].size()) && breakFlag == true) {
+						result = false;
+					}
 				}
 
 			}
