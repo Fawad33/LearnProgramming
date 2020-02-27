2583c373b6060ec1dde4a7c7101248d27d15c32e
Fawadul Hasan <rahathasan706@gmail.com>
2020-02-28 00:58:09 +0600

    - Adds change to valid anagram solution

--- LearnProgramming/ValidAnagram.cpp
+++ LearnProgramming/ValidAnagram.cpp
@@ -6,32 +6,31 @@ using namespace std;
 
 class ValidAnagram {
 public:
+	map<char, int> createMapFromString(string s) {
+		map<char, int> stringMap;
+		for (char i : s) {
+			if (stringMap.find(i) != stringMap.end()) {
+				stringMap[i]++;
+			}
+			else {
+				stringMap[i] = 1;
+			}
+		}
+		return stringMap;
+	}
+
 	bool isAnagram(string s, string t) {
 		map<char, int> sMap, tMap;
 		bool result = true;
 		if (s.size() != t.size()) {
-			result = false;
-			return result;
+			return false;
 		}
-		for (char i : s) {
-			if (sMap.find(i) != sMap.end()) {
-				sMap[i]++;
-			}
-			else {
-				sMap[i] = 1;
-			}
-		}
-		for (char i : t) {
-			if (tMap.find(i) != tMap.end()) {
-				tMap[i]++;
-			}
-			else {
-				tMap[i] = 1;
-			}
-		}
+		sMap = createMapFromString(s);
+		tMap = createMapFromString(t);
 		for (auto it1 = sMap.begin(); it1 != sMap.end(); it1++) {
 			if (tMap.find(it1->first) == tMap.end() || tMap[it1->first] != it1->second) {
 				result = false;
+				break;				
 			}
 		}
 		return result;
