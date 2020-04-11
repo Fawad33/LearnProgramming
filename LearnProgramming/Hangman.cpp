40cabf7102bfc569b339993edfd4ec9bb5140a35
Fawadul Hasan <rahathasan706@gmail.com>
2020-04-12 02:28:59 +0600

    - Adds change to hangman

--- LearnProgramming/Hangman.cpp
+++ LearnProgramming/Hangman.cpp
@@ -1,48 +1,46 @@
 #include<iostream>
 #include<string>
-#include<vector>
 #include<set>
 
 using namespace std;
 
 void checkValidityOfWord(string str, string underscoredStr) {
-	char input;	
+	char input;
 	int attempt = 0;
-	vector<char> wrongAns;
-	set<char> guesses;	
+	set<char> guesses;
 	while (true) {
-		cout << "\n" <<underscoredStr << "\nEnter you guess: ";
-		cin >> input;		
+		cout << "\n" << underscoredStr << "\nEnter you guess: ";
+		cin >> input;
+		bool flag = false;
 		if (guesses.find(input) != guesses.end()) {
 			cout << "\nAlready entered, try something else";
 		}
 		else {
 			guesses.insert(input);
 			for (int i = 0; i < str.size(); i++) {
-				if (str[i] == input) {					
-					for (int i = 0; i < str.size(); i++) {
-						if (str[i] == input) {
-							underscoredStr[i] = input;
-						}
-					}
-					cout << "\nCorrect guess! \nPrevious Inputs: ";
-					for (auto it = guesses.begin(); it != guesses.end(); it++) {
-						cout << *it << " ";
-					}
+				if (str[i] == input) {
+					flag = true;
+					underscoredStr[i] = input;			
 				}
-				else {
-					attempt++;
-					cout << "\nIncorrect guess! \nPrevious Inputs: ";
-					for (auto it = guesses.begin(); it != guesses.end(); it++) {
-						cout << *it << " ";
-					}
-					break;
-				}
-			}		
-		}		
+			}
+			if (flag == false) {
+				attempt++;
+				cout << "\nIncorrect guess! \nPrevious Inputs: ";
+			}
+			else {
+				cout << "\nCorrect guess! \nPrevious Inputs: ";
+			}
+		}
+		for (auto it = guesses.begin(); it != guesses.end(); it++) {
+			cout << *it << " ";
+		}
 		cout << "\nYou have " << (13 - attempt) << " attempts left";
 		if (attempt == 13) {
 			cout << "\nYou lost";
+			break;
+		}
+		if (underscoredStr == str) {
+			cout << "\nYOU WON!";
 			break;
 		}
 	}
