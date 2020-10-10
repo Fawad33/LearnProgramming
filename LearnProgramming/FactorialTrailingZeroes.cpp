369b8f650f93f37490e236839b8037ac2e8d9182
Fawadul Hasan <rahathasan706@gmail.com>
2020-10-11 01:34:09 +0600

    - Adds solution to factorial trailing zeros problem

---
+++ LearnProgramming/FactorialTrailingZeroes.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+class FactorialTrailingZeroes {
+public:
+	int trailingZeroes(int n) {
+		int result = 0;
+
+		if (n == 0)
+			return result;
+
+		while (n >= 5) {
+			result += n / 5;
+			n /= 5;
+		}
+
+		return result;
+	}
+};
+
+void main() {
+	int input = 13;
+	FactorialTrailingZeroes factorialTrailingZeroes;
+	cout << factorialTrailingZeroes.trailingZeroes(input);
+}
--- LearnProgramming/SumOfLeftLeaves.cpp
+++ LearnProgramming/SumOfLeftLeaves.cpp
@@ -31,13 +31,13 @@ public:
 	}
 };
 
-void main() {
-	struct TreeNode* root = new TreeNode(3);
-	root->left = new TreeNode(9);
-	root->right = new TreeNode(20);
-	root->right->left = new TreeNode(15);
-	root->right->right = new TreeNode(7);
-
-	SumOfLeftLeaves sumOfLeftLeaves;
-	cout << sumOfLeftLeaves.sumOfLeftLeaves(root);
-}
+//void main() {
+//	struct TreeNode* root = new TreeNode(3);
+//	root->left = new TreeNode(9);
+//	root->right = new TreeNode(20);
+//	root->right->left = new TreeNode(15);
+//	root->right->right = new TreeNode(7);
+//
+//	SumOfLeftLeaves sumOfLeftLeaves;
+//	cout << sumOfLeftLeaves.sumOfLeftLeaves(root);
+//}
