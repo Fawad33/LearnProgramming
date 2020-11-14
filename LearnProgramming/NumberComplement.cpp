bcdd592d7d802a42a303a64bb891882ddb04ced5
Fawadul Hasan <rahathasan706@gmail.com>
2020-11-14 19:58:00 +0600

    - Adds solution to number complement

--- LearnProgramming/NumberComplement.cpp
+++ LearnProgramming/NumberComplement.cpp
@@ -6,7 +6,7 @@ class NumberComplement {
 public:
 	int findComplement(int num) {
 		int bitLength = floor(log2(num)) + 1;
-		int stringOfOnes = (1 << bitLength) - 1;
+		int stringOfOnes = uint32_t(1 << bitLength) - 1;
 		return num ^ stringOfOnes;
 	}
 };
