0c7ddb1e16c3b4b562326e2710e28e9730d68b3c
fawad33 <rahathasan706@gmail.com>
2022-02-16 00:48:29 +0600

    - adds solution to median of two sorted arrays

--- LearnProgramming/MedianOfTwoSortedArrays.cpp
+++ LearnProgramming/MedianOfTwoSortedArrays.cpp
@@ -1,4 +1,30 @@
 #include<iostream>
-#include<map>
+#include<vector>
+#include <algorithm>
 
 using namespace std;
+
+class MedianOfTwoSortedArrays {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        for (auto i = nums2.begin(); i != nums2.end(); i++) {
+            nums1.push_back(*i);
+        }
+        sort(nums1.begin(), nums1.end());
+        double result = 0;;
+        int size = nums1.size();
+        if (size % 2 == 0) {
+            result = float(float(nums1[(size / 2) - 1] + nums1[size / 2])/2);
+        }
+        else {
+            result = float(float(nums1[size / 2]));
+        }
+        return result;
+    }
+};
+
+void main() {
+    vector<int> nums1 = { 1,2 }, nums2 = { 3,4 };
+    MedianOfTwoSortedArrays medianOfTwoSortedArrays;
+    cout << medianOfTwoSortedArrays.findMedianSortedArrays(nums1, nums2);
+}
